c24ccedcb421c555781b9f45712b987beee57092
belal55 <32621203+belal55@users.noreply.github.com>
2018-04-03 16:16:02 +0600

    E-commerce Nodejs version 1

---
+++ controllers/home.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var productModel = require.main.require('./model/product-model');
+
+router.get('/',function(req,res){
+
+	productModel.getAllProduct(function(result){
+
+		if(req.session.loggedUser==null){
+			res.layout('layouts/visitor/header', {title:"Home"}, {content:{block:"home/index", data:{products:result}}});
+		}else if(req.session.loggedUser[0].user_type_id==1){
+			res.layout('layouts/admin/loggedHeader', {title:"Home"}, {content:{block:"home/index", data:{products:result}}});
+		}else if(req.session.loggedUser[0].user_type_id==2){
+			res.layout('layouts/user/loggedHeader', {title:"Home"}, {content:{block:"home/index", data:{products:result}}});
+		}
+		
+		
+	});
+	
+});
+
+router.post('/',function(req,res){
+
+	productModel.searchProductByName(req.body.searchProductbox,function(result){
+
+		if(req.session.loggedUser==null){
+			res.layout('layouts/visitor/header', {title:"Home"}, {content:{block:"home/index", data:{products:result}}});
+		}else if(req.session.loggedUser[0].user_type_id==1){
+			res.layout('layouts/admin/loggedHeader', {title:"Home"}, {content:{block:"home/index", data:{products:result}}});
+		}else if(req.session.loggedUser[0].user_type_id==2){
+			res.layout('layouts/user/loggedHeader', {title:"Home"}, {content:{block:"home/index", data:{products:result}}});
+		}
+		
+		
+	});
+	
+});
+
+
+module.exports=router;
